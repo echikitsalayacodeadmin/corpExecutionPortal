f3c8b378341b179bb9e1c79c973c8be34b97135d
ayush-uno <dahariaayush@uno.care>
2024-05-30 14:00:55 +0530

    Updated

--- src/landing/landingIndex.jsx
+++ src/landing/landingIndex.jsx
@@ -2,8 +2,36 @@ import { Box, Button, Container, Grid, Stack, Typography } from "@mui/material";
 import BANNERIMG from "../../src/assets/images/banner2.jpg";
 import UNOCARELOGO from "/unocare-logo.png";
 import LoginCorp from "../corpexecutionportal/pages/loginCorp";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const LandingIndex = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  console.log({ navigate, location });
+  const [accessToken, setAccessToken] = useState(false);
+
+  const _accessToken =
+    typeof localStorage !== "undefined"
+      ? localStorage.getItem("AUTHHEADER_CORP_EXECUTION")
+      : null;
+
+  useEffect(() => {
+    setAccessToken(_accessToken ? true : false);
+  }, [_accessToken]);
+
+  useEffect(() => {
+    if (accessToken) {
+      const currentRoute = location.pathname;
+      if (
+        currentRoute === "/" &&
+        localStorage.getItem("AUTHHEADER_CORP_EXECUTION")
+      ) {
+        navigate("/corp/home");
+      }
+    }
+  }, [accessToken, navigate]);
+
   return (
     <Container
       maxWidth={false}
